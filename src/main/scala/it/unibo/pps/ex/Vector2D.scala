33fe8d1b5f5db2e4f36e41896be8c75d41943fdd
mtttia <mattia.senni.03@gmail.com>
2026-03-30 10:37:37 +0200

    feature: Vector2D implementation with 'class' construct

--- src/main/scala/it/unibo/pps/ex/Vector2D.scala
+++ src/main/scala/it/unibo/pps/ex/Vector2D.scala
@@ -25,12 +25,25 @@ trait Vector2D:
 
 object Vector2D:
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = Vector2DImpl(x, y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
   val i: Vector2D = apply(1.0, 0.0) // Unit vector along x-axis
   val j: Vector2D = apply(0.0, 1.0) // Unit vector along y-axis
+
+  private class Vector2DImpl(val x:Double, val y: Double) extends Vector2D {
+
+    override def +(other: Vector2D): Vector2D = Vector2D(x + other.x, y + other.y)
+
+    override def -(other: Vector2D): Vector2D = Vector2D(x - other.x, y - other.y)
+
+    override def *(scalar: Double): Vector2D = Vector2D(x * scalar, y * scalar)
+
+    override def dot(other: Vector2D): Double = x * other.x + y * other.y
+
+    override def magnitude: Double = sqrt(x*x+y*y)
+  }
 
 
 /** Hints:
@@ -69,8 +82,8 @@ object Vector2D:
   println(s"Magnitude of v2: $magV2") // Check if close to 2.236
 
   // Check zero vector and unit vectors if implemented in companion object
-  // println(s"Zero vector: ${Vector2D.zero}")
-  // println(s"Dot product v1.dot(Vector2D.i): ${v1.dot(Vector2D.i)}") // Should be v1.x = 3.0
+  println(s"Zero vector: ${Vector2D.zero}")
+  println(s"Dot product v1.dot(Vector2D.i): ${v1.dot(Vector2D.i)}") // Should be v1.x = 3.0
 
   val multipleOps = (v1 + v2) * 3.0 - Vector2D(1.0, 1.0)
   // sum = (2.0, 6.0)
